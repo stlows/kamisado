19186eddc691a8a331c3aa4aa2fa2a25480ea136
Vincent <vincent_beaulieu@live.ca>
2019-09-24 23:12:48 -0400

    type to store

--- src/store.ts
+++ src/store.ts
@@ -1,30 +1,30 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import Notification from "./models/Notification"
-import Game from './models/Game';
+import Notification from "./models/Notification";
+import Game from "./models/Game";
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
-    state: {
-      notifications : [] as Notification[],
-      loginAs: "cprovost",
-      localGames: [] as Game[]
+  state: {
+    notifications: [] as Notification[],
+    loginAs: "cprovost",
+    localGames: [] as Game[]
+  },
+  mutations: {
+    notify(state: any, notification: Notification) {
+      state.notifications.push(notification);
     },
-    mutations: {
-      notify(state, notification : Notification) {
-        state.notifications.push(notification);
-      },
-      updateLogin(state, updatedLogin){
-        state.loginAs = updatedLogin;
-      },
-      addLocalGame(state, game){
-        state.localGames.push(game);
-      }
+    updateLogin(state: any, updatedLogin: string) {
+      state.loginAs = updatedLogin;
     },
-    getters: {
-      gameById : (state) => (id : number) => {
-        return state.localGames.find((g:Game)  => g.id === id)
-      }
+    addLocalGame(state: any, game: Game) {
+      state.localGames.push(game);
     }
-  })
+  },
+  getters: {
+    gameById: (state: any) => (id: number) => {
+      return state.localGames.find((g: Game) => g.id === id);
+    }
+  }
+});
