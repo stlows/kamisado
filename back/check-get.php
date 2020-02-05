e386b07953a9bd904bbb37bcfe906558686fc3eb
unknown <vincent_beaulieu@live.ca>
2020-02-05 18:15:40 -0500

    remove headers

--- back/check-get.php
+++ back/check-get.php
@@ -1,7 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: 'Origin, X-Requested-With, Content-Type, Accept'");
-header("Content-Type: application/json");
+
 if ($_SERVER['REQUEST_METHOD'] != 'GET') {
   $error = [
     "error" => 5001,
--- src/plugins/api.js
+++ src/plugins/api.js
@@ -3,34 +3,34 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: process.env.VUE_APP_API_URL,
   headers: {
-    'Content-Type': 'application/json; charset=UTF-8'
+    'Content-Type': 'application/json; charset=UTF-8',
   }
 });
 
 export const newGame = (lobbyId) => {
-  return api.post("new-game", { lobbyId })
+  return api.post("new-game.php", { lobbyId })
 }
 
 export const getGame = (gameId) => {
-  return api.get("game?gameId=" + gameId)
+  return api.get("game.php?gameId=" + gameId)
 }
 
 export const getMyGames = () => {
-  return api.get("my-games")
+  return api.get("my-games.php")
 }
 
 export const getTiles = () => {
-  return api.get("tiles")
+  return api.get("tiles.php")
 }
 
 export const getLobby = () => {
-  return api.get("lobby")
+  return api.get("lobby.php")
 }
 
 export const newLobby = (pointsToWin) => {
-  return api.post("new-lobby", { pointsToWin })
+  return api.post("new-lobby.php", { pointsToWin })
 }
 
 export const deleteLobby = (lobbyId) => {
-  return api.post("delete-lobby", { lobbyId })
+  return api.post("delete-lobby.php", { lobbyId })
 }
