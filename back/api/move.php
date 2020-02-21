0b6cddadbcb8bdc8073c1fe8d473d54bbdf253b5
unknown <vincent_beaulieu@live.ca>
2020-02-21 14:00:39 -0500

    validations messages move validations

--- back/api/move.php
+++ back/api/move.php
@@ -4,6 +4,7 @@ include_once("../check-post.php");
 include_once("../check-google-token.php");
 include_once("../sql/sql.php");
 include_once("../prettify.php");
+include_once("../validations.php");
 
 $move = json_decode(file_get_contents('php://input'), true);
 
@@ -22,140 +23,53 @@ $loggedInId = getLoginPlayerId($conn);
 
 // Is it my turn
 if ($game["turn"] != $loggedInId ) {
-  echo (json_encode([
-    "valid" => false,
-    "message" => "It's not your turn."
-  ]));
+  echo (json_encode($NOT_YOUR_TURN));
   exit;
 }
 
 // Is it my color
 if ($tower["player_id"] != $loggedInId ) {
-  echo (json_encode([
-    "valid" => false,
-    "message" => "That is not your tower sir."
-  ]));
+  echo (json_encode($NOT_YOUR_TOWER));
   exit;
 }
 
 // Is it this tower I am supposed to move
 if (!$game["is_first_move"] && $game["tower_id_to_move"] != $towerId) {
-  echo (json_encode([
-    "valid" => false,
-    "message" => "Can't move that tower right now."
-  ]));
+  echo (json_encode($CANT_MOVE_THAT_TOWER));
   exit;
 };
 
 
 if ($targetX < 1 || $targetX > 8 || $targetY < 1 || $targetY > 8) {
-  echo (json_encode([
-    "valid" => false,
-    "message" => "Stay in bound buddy."
-  ]));
+  echo (json_encode($OUT_OF_BOUND));
   exit;
 };
 
 // Is there is a tower already there
 foreach($towers as $t){
   if ($t["position_x"] == $targetX && $t["position_y"] == $targetY) {
-    // TODO Gérer les push ici ?
-    echo (json_encode([
-      "valid" => false,
-      "message" => "There's already a tower there buddy."
-    ]));
+    // TODO Gérer les push de sumo ici ?
+    echo (json_encode($TILE_OCCUPIED));
     exit;
   };
 }
 
-if($tower["player_color"] == "white"){
-  // Is it going forward
-  if($targetY <= $tower["position_y"]){
-    echo (json_encode([
-      "valid" => false,
-      "message" => "You must go forward."
-    ]));
-    exit;
-  }
-  // Different X, must be diagonnally
-  if($targetX != $tower["position_x"]){
-    $deltaX = $targetX - $tower["position_x"];
-    $deltaY = $targetY - $tower["position_y"];
-    if($deltaX != $deltaY){
-      echo (json_encode([
-        "valid" => false,
-        "message" => "You must go forward or diagonnally."
-      ]));
-      exit;
-  }
-  }
-}
-else if($tower["player_color"] == "black"){
-  // Is it going forward
-  if($targetY >= $tower["position_y"]){
-    echo (json_encode([
-      "valid" => false,
-      "message" => "You must go forward."
-    ]));
-    exit;
-  }
-   // Different X, must be diagonnally
-   if($targetX != $tower["position_x"]){
-    $deltaX = $tower["position_x"] - $targetX;
-    $deltaY = $tower["position_y"] - $targetY ;
-    if($deltaX != $deltaY){
-      echo (json_encode([
-        "valid" => false,
-        "message" => "You must go forward or diagonnally."
-      ]));
-      exit;
-  }
-  }
-}else{
-  echo (json_encode([
-    "valid" => false,
-    "message" => "Tower color unknown."
-  ]));
+// Is the tower color white or black
+if($tower["player_color"] != "white" && $tower["player_color"] != "black"){
+  echo (json_encode($TOWER_COLOR_UNKNOWN));
   exit;
 }
 
+// Is it going forward
+if(($tower["player_color"] == "white" && $targetY <= $tower["position_y"]) || ($tower["player_color"] == "black" && $targetY >= $tower["position_y"])){
+  echo (json_encode($MUST_GO_FORWARD));
+  exit;
+}
 
+// If it's a different X, is it in the same diagonal
+if($targetX != $tower["position_x"] && ($targetX - $tower["position_x"] != $targetY - $tower["position_y"])){
+  echo (json_encode($MUST_GO_DIAGONNALLY_OR_STRAIGHT_AHEAD));
+  exit;
+}
 
-
-
-
-
-// setPossibleTilesVertically(tower) {
-//   let tile = this.getTileByTower(tower);
-//   let y = this.nextY(tile.y, tower.playerColor);
-//   let counter = 1;
-//   while (
-//     this.inBound(tile.x, y) &&
-//     (!this.getTileByCoord(tile.x, y).tower || this.canPush(tower, this.getTileByCoord(tile.x, y).tower) ) &&
-//     counter <= this.maxTilesBySumo[tower.sumo]
-//   ) {
-//     this.getTileByCoord(tile.x, y).selectable = true;
-//     y = this.nextY(y, tower.playerColor);
-//     counter++;
-//   }
-// }
-// setPossibleTilesDiagonnally(tower, deltaX) {
-//   let tile = this.getTileByTower(tower);
-//   let x = tile.x + deltaX;
-//   let y = this.nextY(tile.y, tower.playerColor);
-//   let counter = 1;
-//   while (
-//     this.inBound(x, y) &&
-//     !this.getTileByCoord(x, y).tower &&
-//     counter <= this.maxTilesBySumo[tower.sumo]
-//   ) {
-//     this.getTileByCoord(x, y).selectable = true;
-//     y = this.nextY(y, tower.playerColor);
-//     x = x + deltaX;
-//     counter++;
-//   }
-// }
-echo (json_encode([
-  "valid" => true,
-  "message" => "Move is valid."
-]));
+echo (json_encode($VALID));
---
+++ back/validations.php
@@ -0,0 +1,51 @@
+<?php
+
+$NOT_YOUR_TURN = [
+  "valid" => false,
+  "message" => "It's not your turn."
+];
+
+$NOT_YOUR_TOWER = [
+  "valid" => false,
+  "message" => "That is not your tower sir."
+];
+
+$CANT_MOVE_THAT_TOWER = [
+  "valid" => false,
+  "message" => "Can't move that tower right now."
+];
+
+$OUT_OF_BOUND = [
+  "valid" => false,
+  "message" => "Stay in bound buddy."
+];
+
+$TILE_OCCUPIED = [
+  "valid" => false,
+  "message" => "There's already a tower there buddy."
+];
+
+$MUST_GO_FORWARD = [
+  "valid" => false,
+  "message" => "You can't go back."
+];
+
+$MUST_GO_DIAGONNALLY_OR_STRAIGHT_AHEAD = [
+  "valid" => false,
+  "message" => "You must go diagonnally or straight ahead."
+];
+
+$TOWER_COLOR_UNKNOWN = [
+  "valid" => false,
+  "message" => "Tower color unknown."
+];
+
+$INVALID = [
+  "valid" => false,
+  "message" => "Move is invalid."
+];
+
+$VALID = [
+  "valid" => true,
+  "message" => "Move is valid."
+];
